884dd7a92441319b26576228d47bd4971cae5827
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2021-11-30 02:48:21 +0800

    First commit(Making a program to count the words in the sentence)

---
+++ Assignment 7(1).py
@@ -0,0 +1,13 @@
+def DisplayingWordsVowellsConstant ():
+    import string
+    Words=0
+    Vowells=0
+    Consonants=0
+    Sentence = input("Give a sentence: ")
+    
+    
+
+
+
+
+DisplayingWordsVowellsConstant()
