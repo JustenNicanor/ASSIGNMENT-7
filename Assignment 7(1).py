52c3711f96fbd794a1bc7abf908acadef779bffe
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2021-11-30 07:03:09 +0800

    First commit(Code to count each word)

--- Assignment 7(1).py
+++ Assignment 7(1).py
@@ -1,9 +1,24 @@
 def DisplayingWordsVowellsConstant ():
     import string
+    letters=0
     Words=0
     Vowells=0
     Consonants=0
     Sentence = input("Give a sentence: ")
+    sentencelength = len(Sentence)
+    for i in range(sentencelength):
+        if Sentence[i] ==" " :
+            if letters != 0:
+                Words+=1
+        else:
+            letters+=1
+    if letters != 0:
+        Words+=1
+            
+        
+    print(f"The number of word is {Words}.")
+
+
     
     
 
