e31771bb5c0d7d2900e5b6b0d42c77c2dc1fabd5
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2021-11-30 19:20:19 +0800

    Third commit(Changes in lines, and adding a code to count the Vowells and Constant)

--- Assignment 7(1).py
+++ Assignment 7(1).py
@@ -1,5 +1,4 @@
 def DisplayingWordsVowellsConstant ():
-    import string
     letters=0
     Words=0
     Vowells=0
@@ -12,14 +11,25 @@ def DisplayingWordsVowellsConstant ():
                 Words+=1
         else:
             letters+=1
+            if Sentence[i] == "A" or Sentence[i] == "a":        
+                Vowells+=1
+            elif Sentence[i] == "E" or Sentence[i] == "e":
+                Vowells+=1
+            elif Sentence[i] == "I" or Sentence[i] == "i":
+                Vowells+=1
+            elif Sentence[i] == "O" or Sentence[i] == "o":
+                Vowells+=1
+            elif Sentence[i] == "U" or Sentence[i] == "u":  
+                Vowells+=1
+            else:
+                Consonants+=1
     if letters != 0:
         Words+=1
-            
-        
-    print(f"The number of word is {Words}.")
-
-
     
+    
+    print(f"WORDS: {Words}")
+    print(f"VOWELLS: {Vowells}")
+    print(f"CONSONANTS: {Consonants}")
     
 
 
